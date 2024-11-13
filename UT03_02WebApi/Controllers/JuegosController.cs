6c1e75502d8748e0c86996e51078d6db08afd776
2dawa <2dawa@informatica.edu>
2024-11-13 09:53:28 +0000

    Me faltan request

---
+++ UT03_02WebApi/Controllers/JuegosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UT03_02WebApi.Models;
+
+namespace UT03_02WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JuegosController : ControllerBase
+    {
+
+        private static List<Juego> Juegos = new List<Juego>()
+        {
+               new Juego {
+                   Id = 1,
+                   Genre = "Guerra",
+                   Title = "WarHammer"
+        },
+            new Juego {
+                Id = 2,
+                Genre = "Deporte",
+                Title = "Fifa"
+        },
+            new Juego {
+                Id=3,
+                Genre="Aventura",
+                Title="Last of us"
+            }
+
+        };
+
+        private readonly ILogger<JuegosController> _logger;
+
+        public JuegosController(ILogger<JuegosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetJuego")]
+        public IActionResult Get()
+        {
+            return Ok(Juegos.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var game= Juegos.Find(a => a.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+        
+            return Ok(game);  
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Juego juego)
+        {
+            int idJuego=Juegos.Last().Id;
+            if (juego == null)
+            {
+                return NotFound();
+            }
+            juego.Id = idJuego+1;
+            Juegos.Add(juego);
+            return CreatedAtAction(nameof(Get), nameof(JuegosController), juego);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,[FromBody] Juego juego)
+        {
+            Juego game=Juegos.Find(a => a.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            game.Id = juego.Id;
+            game.Title = juego.Title;
+            game.Genre=juego.Genre;
+            return Ok(game);
+        }
+    }
+}
---
+++ UT03_02WebApi/Models/Juego.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UT03_02WebApi.Models
+{
+    public class Juego
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Titulo")]
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "El titulo debe tener entre {2} y {1} caracteres.")]
+        public string Title { get; set; }
+        [DisplayName("Genero")]
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        [StringLength(12, MinimumLength = 2, ErrorMessage = "El genero debe tener entre {2} y {1} caracteres.")]
+        public string Genre { get; set; }
+    }
+}
